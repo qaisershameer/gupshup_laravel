38a2a03dfda510f84fa2f472d1ece68d40beb277
Qaiser Shameer <naeem.malik009@gmail.com>
2024-10-10 08:33:35 +0500

    10 Oct Home

--- resources/views/admin/sidebar.blade.php
+++ resources/views/admin/sidebar.blade.php
@@ -4,8 +4,8 @@
         <div class="sidebar-header d-flex align-items-center">
           <div class="avatar"><img src="admin/img/avatar-6.jpg" alt="..." class="img-fluid rounded-circle"></div>
           <div class="title">
-            <h1 class="h5">Mark Stephen</h1>
-            <p>Web Designer</p>
+            <h1 class="h5">Qaiser Shameer</h1>
+            <p>App Developer</p>
           </div>
         </div>
         <!-- Sidebar Navidation Menus--><span class="heading">Main</span>
@@ -13,12 +13,18 @@
                 <li class="active"><a href="index.html"> <i class="icon-home"></i>Home </a></li>
                 <li><a href="tables.html"> <i class="icon-grid"></i>Tables </a></li>
                 <li><a href="charts.html"> <i class="fa fa-bar-chart"></i>Charts </a></li>
-                <li><a href="forms.html"> <i class="icon-padnote"></i>Forms </a></li>
-                <li><a href="#exampledropdownDropdown" aria-expanded="false" data-toggle="collapse"> <i class="icon-windows"></i>Example dropdown </a>
+                <li><a href="forms.html"> <i class="icon-padnote"></i>Orders </a></li>
+                <li><a href="#exampledropdownDropdown" aria-expanded="false" data-toggle="collapse"> <i class="icon-windows"></i>GupShup Menu </a>
                   <ul id="exampledropdownDropdown" class="collapse list-unstyled ">
-                    <li><a href="#">Page</a></li>
-                    <li><a href="#">Page</a></li>
-                    <li><a href="#">Page</a></li>
+                    <li><a href="#">BAR B Q</a></li>
+                    <li><a href="#">CHINESE</a></li>
+                    <li><a href="#">PAKISTANI</a></li>
+                    <li><a href="#">KARAHI</a></li>
+                    <li><a href="#">TANDORI</a></li>
+                    <li><a href="#">PATILA</a></li>
+                    <li><a href="#">FISH</a></li>
+                    <li><a href="#">PIZZA</a></li>
+                    <li><a href="#">BURGERS</a></li>
                   </ul>
                 </li>
                 <li><a href="login.html"> <i class="icon-logout"></i>Login page </a></li>
