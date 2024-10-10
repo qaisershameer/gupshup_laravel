775ef4f06e25c6463f908117dc711fd4b06e18fe
Qaiser Shameer <naeem.malik009@gmail.com>
2024-10-10 08:04:21 +0500

    10 Oct Home

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\User;
+
+use Illuminate\Support\Facades\Auth;
+
+class HomeController extends Controller
+{
+
+    public function my_home()
+    {
+        return view('home.index');
+    }
+
+    public function index()
+    {
+        if(Auth::id())
+        {
+            $usertype = Auth()->user()->usertype;
+
+            if($usertype == 'admin')
+            {
+                return view('admin.index');
+            }
+            else
+            {
+                return view('home.index');
+            }
+
+        }
+    }
+}
---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    @include('home.css')
+
+</head>
+<body data-spy="scroll" data-target=".navbar" data-offset="40" id="home">
+    
+
+    @include('home.header')
+
+    @include('home.about')
+    
+    @include('home.gallery')  
+    
+    @include('home.booktable')
+
+    @include('home.blog')
+
+    @include('home.reviews')
+
+    @include('home.contact')
+
+    @include('home.footer')    
+
+	<!-- core  -->
+    <script src="assets/vendors/jquery/jquery-3.4.1.js"></script>
+    <script src="assets/vendors/bootstrap/bootstrap.bundle.js"></script>
+
+    <!-- bootstrap affix -->
+    <script src="assets/vendors/bootstrap/bootstrap.affix.js"></script>
+
+    <!-- wow.js -->
+    <script src="assets/vendors/wow/wow.js"></script>
+    
+    <!-- google maps -->
+    <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCtme10pzgKSPeJVJrG1O3tjR6lk98o4w8&callback=initMap"></script>
+
+    <!-- FoodHut js -->
+    <script src="assets/js/foodhut.js"></script>
+
+</body>
+</html>
